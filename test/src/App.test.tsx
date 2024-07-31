6334905edeb0b7e8b1227574bc555422e41eca65
Leonardo Santino <leonardosantino@live.com>
2024-07-30 21:34:03 -0300

    test: app

--- test/src/App.test.tsx
+++ test/src/App.test.tsx
@@ -1,13 +1,13 @@
 import '../utils';
 
-// import { render } from '@testing-library/react';
-//
-// import { App } from '../../src/App';
+import { render } from '@testing-library/react';
+
+import { App } from '../../src/App';
 
 describe('App', () => {
   it('should render header', async () => {
-    // const component = render(<App />).container;
-    //
-    // expect(component).toHaveTextContent('App works!');
+    const component = render(<App />).container;
+
+    expect(component).toHaveTextContent('App works!');
   });
 });
